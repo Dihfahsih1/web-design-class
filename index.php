dbf0a642686af0cb776299e62bab216621727399
Dihfahsih1 <dihfahsihm@gmail.com>
2024-07-26 16:04:59 +0300

    decision making and for loop

--- index.php
+++ index.php
@@ -1,5 +1,27 @@
 <?php
 
-    echo "hello, world";
-    
+$name=2092929;
+$age=30;
+$height=5.6;
+$is_student = true;
+
+$score=165;
+// if ($score>=90){
+//     echo "Grade A";
+//     }
+// elseif ($score>=80){
+//     echo "Grade B";
+// }
+
+// elseif ($score>=70){
+//     echo "Grade C";
+// }
+// else{
+//     echo "Grade F";
+// }
+// We use for loop when we know the number of times the task will execute
+
+for ($i=-2; $i<=5; $i++){
+    echo "The number is: $i <br>";
+}
 ?>
